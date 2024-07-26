021b113396c79039e4675638c35f04ff3e5f1b82
Joshua Rogers <joshuarogers857@gmail.com>
2024-07-26 11:45:47 +0100

    Initial commit

---
+++ api/app/Http/Controllers/ToDoListController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ToDoList;
+use Illuminate\Http\Request;
+
+class ToDoListController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ToDoList $toDoList)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ToDoList $toDoList)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ToDoList $toDoList)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ToDoList $toDoList)
+    {
+        //
+    }
+}
