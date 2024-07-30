4bd01e57158dffe93b0ff6049edfc1f2269f6194
Joshua Rogers <joshuarogers857@gmail.com>
2024-07-30 14:13:15 +0100

    Add ToDoItem routes

--- api/app/Http/Controllers/ToDoItemController.php
+++ api/app/Http/Controllers/ToDoItemController.php
@@ -3,63 +3,69 @@
 namespace App\Http\Controllers;
 
 use App\Models\ToDoItem;
+use App\Models\ToDoList;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ToDoItemController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(string $toDoListId): Response
     {
-        //
-    }
+        $toDoList = ToDoList::findOrFail($toDoListId);
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        $toDoItems = $toDoList->items()->get();
+
+        return response()->json($toDoItems, Response::HTTP_OK);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, string $toDoListId): Response
     {
-        //
-    }
+        if (!ToDoList::where("id", $toDoListId)->exists()) {
+            return response()->json(['error' => 'ToDoList not found'], Response::HTTP_NOT_FOUND);
+        }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(ToDoItem $toDoItem)
-    {
-        //
-    }
+        $data = $request->validate([
+            "content" => ["required", "string", "max:200"],
+            "is_complete" => ["required", "boolean"],
+        ]);
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(ToDoItem $toDoItem)
-    {
-        //
+        $data["to_do_list_id"] = $toDoListId;
+        $toDoItem = ToDoItem::create($data);
+
+        return response()->json($toDoItem, Response::HTTP_CREATED);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, ToDoItem $toDoItem)
+    public function update(Request $request, string $listId, string $itemId): Response
     {
-        //
+        $toDoItem = ToDoItem::findOrFail($itemId);
+
+        $data = $request->validate([
+            "to_do_list_id" => ["required", "int", "exists:to_do_lists,id"],
+            "content" => ["required", "string", "max:200"],
+            "is_complete" => ["required", "boolean"],
+        ]);
+
+        $toDoItem->update($data);
+
+        return response()->json($toDoItem, Response::HTTP_OK);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(ToDoItem $toDoItem)
+    public function destroy(string $listId, string $itemId): Response
     {
-        //
+        $result = ToDoItem::destroy($itemId);
+
+        return request()->json($result, Response::HTTP_NO_CONTENT);
     }
 }
--- api/routes/api.php
+++ api/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ToDoListController;
+use App\Http\Controllers\ToDoItemController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,3 +14,8 @@ Route::get('/todos', [ToDoListController::class, 'index'])->name('todolist.index
 Route::get('/todos/{id}', [ToDoListController::class, 'show'])->name('todolist.show');
 Route::put('/todos/{id}', [ToDoListController::class, 'update'])->name('todolist.update');
 Route::delete('/todos/{id}', [ToDoListController::class, 'destroy'])->name('todolist.destroy');
+
+Route::post('/todos/{id}/items', [ToDoItemController::class, 'store'])->name('todoitem.store');
+Route::get('/todos/{id}/items', [ToDoItemController::class, 'index'])->name('todoitem.index');
+Route::put('/todos/{listId}/items/{itemId}', [ToDoItemController::class,'update'])->name('todoitem.update');
+Route::delete('/todos/{listId}/items/{itemId}', [ToDoItemController::class, 'destroy'])->name('todoitem.destroy');
